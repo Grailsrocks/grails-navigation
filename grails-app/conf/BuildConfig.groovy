04ae039e199c0eccf2f3050aa48f77ff01a83ef6
Luca Orlandi <luca.orlandi@gmail.com>
2014-07-31 17:44:30 +0200

    Added release to locally deploy artifact

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,27 +2,27 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.project.fork = false
+
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-    }
-    log "warn"
-    legacyResolve false
+
+    inherits("global")
+
     repositories {
-        grailsHome()
         grailsCentral()
+        mavenLocal()
         mavenCentral()
     }
     dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.5'
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-                ":release:2.2.1",
-                ":rest-client-builder:1.0.3"
-        ) {
+        build ':release:3.0.1', {
             export = false
         }
     }
